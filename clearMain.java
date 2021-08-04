b0d53ec0374e4d5c85e0296fed7bdbf17b9006b5
INFORMATIQUE <ominkoua@menbat.local>
2021-08-04 09:46:05 +0200

    Add security to the automatic delete

--- clearMain.java
+++ clearMain.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 public class clearMain {
     
     public static void main(String[] args) {
