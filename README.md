db75fc5bcdd1686d359b3499d16457cff491f46e
Perzival <dylan.madelaine3@gmail.com>
2023-01-01 22:27:35 +0100

    Base

--- README.md
+++
@@ -1,9 +0,0 @@
-# setup-iracing
-
-Utilitaire java pour organiser des fichiers de configuration iracing
-
-## Notes
-
-Pour pouvoir l'utiliser il faut dl le .jar, ensuite il faut changer le config.txt en mettant le lien qu'il faut pour aller jusqu'a iracing
-ensuite il faut créer le dossier Donnée a coté d'iracing, ensuite mettre le .jar et le config.txt a coté, double clic sur le .jar et magie les setups sont dans les bon dossiers.
-Pour utilisé le delete, il faut mettre le jar a coté de config et setup-iracing.jar. Double clic et magie tout repars.
--- setup-iracing/src/main/java/Main.java
+++ setup-iracing/src/main/java/Main.java
@@ -1,12 +1,12 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -201,7 +201,8 @@ public class Main{
             for(String key:hmap.keySet()){
                 if(check_pattern(key, i)) remplissage(line,hmap.get(key), s, i,setup);
             }   
-        }       
+        }
+
     }
 
 
